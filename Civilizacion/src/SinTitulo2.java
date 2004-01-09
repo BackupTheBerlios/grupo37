dd1128ad5ec3f599ca3f3e741ae7fc683b5a2fd3
alexbm7 <alexbm7>
2004-01-09 23:58:52 +0000

    <No se introdujeron comentarios>

---
+++ Civilizacion/src/SinTitulo2.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * <p>Título: </p>
+ * <p>Descripción: </p>
+ * <p>Copyright: Copyright (c) 2004</p>
+ * <p>Empresa: </p>
+ * @author sin atribuir
+ * @version 1.0
+ */
+
+public class SinTitulo2 {
+  public SinTitulo2() {
+  }
+fhfghfghfghf
+}
